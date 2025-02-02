760e346b88ab78f02c7c958ca85a8fe9e398c5df
amielll <amielreyes471@gmail.com>
2025-02-03 00:05:06 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex flex-col">
       <section className="bg-blue-500 p-12 md:p-6 sm:p-4 min-h-[90vh] flex justify-center items-center font-bold">
-        <h1 className="text-4xl sm:text-2xl text-red-600">Home</h1>
+        <h1 className="text-4xl sm:text-2xl text-red-600">Home page</h1>
       </section>
 
       <section className="bg-white p-12 md:p-6 sm:p-4 min-h-[90vh] flex justify-center items-center">
