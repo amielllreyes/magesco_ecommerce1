6e0864c87989120957c3e880c5997fdfacb2a0b8
amielll <amielreyes471@gmail.com>
2025-02-02 23:52:29 +0800

    First Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col">
+      <section className="bg-blue-500 p-12 md:p-6 sm:p-4 min-h-[90vh] flex justify-center items-center font-bold">
+        <h1 className="text-4xl sm:text-2xl text-red-600">Home</h1>
+      </section>
+
+      <section className="bg-white p-12 md:p-6 sm:p-4 min-h-[90vh] flex justify-center items-center">
+        <h1 className="text-4xl sm:text-2xl">Images</h1>
+      </section>
+    </main>
+  );
+}
